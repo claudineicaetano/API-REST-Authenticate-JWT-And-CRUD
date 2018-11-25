3d419b5100bb9dfcbbe8a292ef639568a416b385
Claudinei Caetano de Souza <claudinei@angularlabs.com.br>
2018-11-25 16:07:05 -0200

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -26,46 +26,4 @@ app.listen(3000, () => {
   console.log('Start on port 3000');
 });
 
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-// var Todo = mongoose.model('Todo', {
-//   text: {
-//     type: String,
-//     required: true,
-//     minlength: 1,
-//     trim: true
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false
-//   },
-//   completedAt: {
-//     type: Number,
-//     default: null
-//   }
-// });
-
-
-// // var otherTodo = new Todo({
-// //  text: 'Something to do'
-// // });
-
-// // otherTodo.save().then((doc) => {
-// // console.log(JSON.stringify(doc, undefined, 2));
-// // }, (e) => {
-// //     console.log('Unable to save todo');
-// // });
-
-
-
-// var user = new User({
-//   email: 'claudinei@gmail.com'
-// });
-
-// user.save().then((doc) =>{
-//   console.log('user saved', doc);
-// }, (e) => {
-//     console.log('Unable to save User');
-// });
+module.exports = {app};
