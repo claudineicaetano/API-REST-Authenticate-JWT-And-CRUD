5c059b86aa3b9bc525309f0fa589542734a9be2f
Claudinei Caetano de Souza <claudinei@angularlabs.com.br>
2018-11-25 20:07:58 -0200

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,43 @@ describe('GET /todos/:id', () => {
 
   });
 });
+
+describe('DELETE /todos/:id', (done) =>{
+  it('should remove a todo', (done) =>{
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findByIdAndDelete(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) =>{
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+    .delete('/todos/1223er')  
+    .expect(404)
+    .end(done);
+
+  });
+});
